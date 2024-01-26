99b794c5c3cc96f9167152702847d2d03120dad5
rachit-j <rjaiswal.sd.77@gmail.com>
2024-01-26 12:31:22 -0800

    Update localcopy.sh

--- localcopy.sh
+++ localcopy.sh
@@ -2,7 +2,7 @@
 
 # If this script has a permission denied error, run chmod +x localcopy.sh
 
-echo "Starting/Restarting the codemaxxer backend on the localhost. Please make sure you are running this script from the source of your project, or in this case, the ww3-backend folder. (cd ___/____/____/ww3-backend/)"
+echo "Starting/Restarting the codemaxxer backend on the localhost. Please make sure you are running this script from the source of your project, or in this case, the codemaxxerBackend folder. (cd ___/____/____/codemaxxerBackend/)"
 echo -n "Are you ready [PRESS ENTER TO CONTINUE]"
 read hello
 echo "----------------------------------------------------------------------------------------------------------------"
