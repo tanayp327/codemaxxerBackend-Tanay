4fe9ba82805191ab3384d81533eb939bf5c18d63
TheoH32 <theo.h131@gmail.com>
2024-02-03 23:48:37 -0800

    signout testing

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 				)
 				// list the requests/endpoints need to be authenticated
 				.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/authenticate").permitAll()
+					.requestMatchers("/authenticate", "/signout").permitAll()
 					.requestMatchers("/reading").hasAnyAuthority("ROLE_ADMIN")
 					.requestMatchers("/mvc/person/update/**", "/mvc/person/delete/**").hasAnyAuthority("ROLE_ADMIN")
 					.requestMatchers("/api/person/delete/**").hasAnyAuthority("ROLE_ADMIN")
