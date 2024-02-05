b1045e5097bebf83be9f4f02e770a41b75d4e60e
tanishapatil1234 <tanishapatil07@gmail.com>
2024-02-05 11:20:55 -0800

    Game Panel Map and Sprites

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/collision/Entity.java
@@ -0,0 +1,10 @@
+package com.nighthawk.spring_portfolio.mvc.collision;
+
+
+// Parent class for all character classes
+public class Entity {
+    public int x, y;  // position
+    public int speed;  // speed 
+
+    public String direction; 
+}
--- src/main/java/com/nighthawk/spring_portfolio/mvc/collision/GamePanel.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/collision/GamePanel.java
@@ -1,43 +1,52 @@
 package com.nighthawk.spring_portfolio.mvc.collision;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 
-
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
-    //Screen Settings 
-    final int originalTileSize = 16;  // 16x16 default tile/character size 
-    // scaling 
+    //Screen Settings
+    final int originalTileSize = 16;  // 16x16 default tile/character size
+    // scaling
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale ; 
-    // 16 x 12 map 
-    final int maxScreenCol = 16; 
-    final int maxScreenRow = 12; 
+    public final int tileSize = originalTileSize * scale;
+    // 16 x 12 map
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
 
-    final int screenWidth = tileSize * maxScreenCol;  // 768 pixels 
-    final int screenHeight = tileSize * maxScreenRow;  // 576 pixels 
+    final int screenWidth = tileSize * maxScreenCol;  // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow;  // 576 pixels
 
-    KeyHandler keyH = new KeyHandler(); 
-    Thread gameThread; 
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
 
-    // Setting player default position 
+    // Setting player default position
     int playerX = 100;
-    int playerY = 100; 
-    int playerSpeed = 4; 
+    int playerY = 100;
+    int playerSpeed = 4;
 
+    // NPC 
+    
 
-    // FPS 
-    int FPS = 60; 
+    // FPS
+    int FPS = 50;
 
+    private Image backgroundImage;
 
     public GamePanel() {
+        Npc npc = new Npc(200, 200, "Hello, I am an NPC!", this);
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.black);
+        this.addMouseListener(npc);
+
+        // Load your background image
+        this.backgroundImage = new ImageIcon(getClass().getResource("/com/nighthawk/spring_portfolio/mvc/collision/sprites/gamemap.png")).getImage();
+
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
@@ -46,65 +55,52 @@ public class GamePanel extends JPanel implements Runnable{
     public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
-
     }
 
     @Override
     public void run() {
-        
-        double drawInterval = 1000000000/FPS; // screen is redrawn 60 times per sec
-        double nextDrawTime = System.nanoTime() + drawInterval; 
-        while(gameThread != null) {
+        double drawInterval = 1000000000 / FPS; // screen is redrawn 60 times per sec
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        while (gameThread != null) {
 
-            
-            // 1- UPDATE character info like position 
+            // 1- UPDATE character info like position
             update();
             // 2 - DRAW Screen with updated information
             repaint();
-            
-        try {
-            double remainingTime = nextDrawTime - System.nanoTime() ; 
-            // convert nano to milli
-            remainingTime = remainingTime/1000000;
 
-            if(remainingTime < 0) {
-                remainingTime = 0;
-            }
-            
-            Thread.sleep((long) remainingTime);
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                // convert nano to milli
+                remainingTime = remainingTime / 1000000;
 
-            nextDrawTime += drawInterval;
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
 
-        } catch (InterruptedException e){
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
     public void update() { // player position updating
-        if(keyH.upPressed == true) {
-            playerY -= playerSpeed;   
-        } 
-        else if(keyH.downPressed == true) {
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        // Draw the background image
+        g2.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+
+        // Draw the player on top of the background
+        player.draw(g2);
         g2.dispose();
     }
-
-    
 }
